8cbf0471acc63147a27c6bf2cc92fe183c7b2fef
Ankur Vekariya <avekariya@cloudbees.com>
2024-09-25 12:05:04 +0530

    xcode project setup for ios fm example app

---
+++ swift-fm-exampleTests/swift_fm_exampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  swift_fm_exampleTests.swift
+//  swift-fm-exampleTests
+//
+//  Created by Ankur Vekariya on 25/09/24.
+//
+
+import Testing
+
+struct swift_fm_exampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
