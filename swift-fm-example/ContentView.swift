22964ab5e8d05337a508137d1914633f8032ec29
Ankur <avekariya@cloudbees.com>
2024-09-26 18:59:28 +0530

    added content file for sample UI

--- swift-fm-example/ContentView.swift
+++ swift-fm-example/ContentView.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+enum TitleColor: String {
+    case white = "White"
+    case blue = "Blue"
+    case green = "Green"
+    case yellow = "Yellow"
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -16,7 +23,28 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear{
+            flags.INSTANCE.enableTutorial.enabled {
+              // TODO: Put your code here that needs to be gated
+            }
+            
+            print("color == \(flags.INSTANCE.titleColors)")
+//            switch flags.INSTANCE.titleColors {
+//              case "White":
+//                print("Title color is White")
+//              case "Blue":
+//                print("Title color is Blue")
+//              case "Green":
+//                print("Title color is Green")
+//              case "Yellow":
+//                print("Title color is Yellow")
+//              default:
+//                print("Title color is default - White")
+//            }
+            
+         }
     }
+
 }
 
 #Preview {
