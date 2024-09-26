674b26cbce96ad3cf75a3b6608477c7bdf8bfd3d
Ankur <avekariya@cloudbees.com>
2024-09-26 18:51:00 +0530

    initialized ROX SDK

--- swift-fm-example/swift_fm_exampleApp.swift
+++ swift-fm-example/swift_fm_exampleApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct swift_fm_exampleApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
