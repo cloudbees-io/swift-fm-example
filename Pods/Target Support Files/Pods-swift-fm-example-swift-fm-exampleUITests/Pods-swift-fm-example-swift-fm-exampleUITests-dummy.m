ac1552c5f8caf5d8e08e63e0a0807a13b62a26ff
Ankur <avekariya@cloudbees.com>
2024-09-30 15:51:44 +0530

    removed pods folder from git

--- Pods/Target Support Files/Pods-swift-fm-example-swift-fm-exampleUITests/Pods-swift-fm-example-swift-fm-exampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swift_fm_example_swift_fm_exampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_swift_fm_example_swift_fm_exampleUITests
-@end
--- Pods/Target Support Files/Pods-swift-fm-exampleTests/Pods-swift-fm-exampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swift_fm_exampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_swift_fm_exampleTests
-@end
